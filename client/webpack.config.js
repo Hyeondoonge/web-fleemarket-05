8f0ff4dd5aa16540c574ae7bcb431c07c5eaf91e
HyeonDoonge <55647436+Hyeondoonge@users.noreply.github.com>
2022-08-17 21:55:55 +0900

    개발 환경 및 프로젝트 설정 (#2)
    * docs(template): Commit template 수정
    
    - Commit template에 Collaborator 추가
    
    Co-authored-by: Hyeondoonge <jsi06138@gmail.com>
    
    * chore: yarn workspace 설정
    
    - yarn workspace 설정
    
    - #1
    
    Co-authored-by: Hyeondoonge <jsi06138@gmail.com>
    
    * chore: NestJS 설정
    
    - nestjs 환경 설정
    - 공통된 dependency는 루트에서 관리하도록 설정
    
    - #1
    
    Co-authored-by: Hyeondoonge <jsi06138@gmail.com>
    
    * docs(snippet): React 관련 스니펫 추가
    
    - React Functional Component 스니펫 추가
    - React Styled Component Theme 관련 추가
    
    Co-authored-by: Hyeondoonge <jsi06138@gmail.com>
    
    * chore: React Webpack 설정
    
    - React Webpack 관련 설정
    - Babel 설정
    - ESLint 관련 설정
    
    - #1
    
    Co-authored-by: Hyeondoonge <jsi06138@gmail.com>
    
    * chore: husky, lint-staged 관련 설정 추가
    
    - husky 및 lint-staged 관련 설정 추가
    - prettier 설정 파일 이름 변경
    
    - #1
    
    Co-authored-by: Hyeondoonge <jsi06138@gmail.com>
    
    Co-authored-by: sangrimlee <contact@sangrim.dev>

---
+++ client/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.[name].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            issuer: /\.[jt]sx?$/,
+            use: [
+              {
+                loader: '@svgr/webpack',
+                options: { icon: true, typescript: true },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+      favicon: path.join(__dirname, 'public', 'favicon.ico'),
+    }),
+  ],
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:4000',
+    },
+  },
+};
