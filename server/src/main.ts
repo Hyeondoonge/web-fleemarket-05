e9ccdabc8114d7b5a590a4b7ffeb09e77d45c0df
이상림 (Sang Rim Lee) <contact@sangrim.dev>
2022-08-18 18:11:53 +0900

    Swagger 관련 설정 추가 (#12)
    - Swagger 관련 설정 추가
    - Issue: #10

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { documentConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  app.setGlobalPrefix('api');
 
   const port = configService.get<number>('PORT');
   await app.listen(port);
